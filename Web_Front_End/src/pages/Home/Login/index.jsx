59905bf302cb724e5578c8f50bfabeae6cf6a16d
XieSenHao <983298384@qq.com>
2020-12-14 00:40:28 +0800

    update

--- Web_Front_End/src/pages/Home/Login/index.jsx
+++ Web_Front_End/src/pages/Home/Login/index.jsx
@@ -1,15 +1,43 @@
 import { NavLink } from "react-router-dom";
+import { Form, Input } from "antd";
 import "./index.css";
 
 function Login(props) {
   const navToHome = function () {
     props.history.push("/Doc");
   };
+  const formItemLayout = {
+    wrapperCol: {
+      sm: { offset: 3 },
+    },
+  };
   return (
     <div id="subBox">
       <div id="loginText">登录</div>
-      <input type="text" name="" id="account" placeholder="账号" />
-      <input type="text" name="" id="password" placeholder="请输入内容" />
+      <Form {...formItemLayout} layout="horizontal">
+        <Form.Item
+          name="account"
+          rules={[
+            {
+              required: true,
+              message: "请输入你的账号！",
+            },
+          ]}
+        >
+          <Input placeholder="账号" />
+        </Form.Item>
+        <Form.Item
+          name="password"
+          rules={[
+            {
+              required: true,
+              message: "请输入你的密码！",
+            },
+          ]}
+        >
+          <Input placeholder="密码" />
+        </Form.Item>
+      </Form>
       <p id="fg_pw">忘记密码</p>
       <button id="loginBtn" onClick={navToHome}>
         登录
