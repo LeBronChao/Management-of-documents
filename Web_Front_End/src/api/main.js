c4a5d3531477119b07e48000d33f143118d93d00
LeBronChao <1531229587@qq.com>
2020-12-15 01:31:08 +0800

    12.15_cww

--- Web_Front_End/src/api/main.js
+++ Web_Front_End/src/api/main.js
@@ -3,14 +3,24 @@ import axios from 'axios'
 const PROTOCOL = 'http'
 const ADDRESS = '10.118.21.172'
 const PORT = 3001
-
-axios.defaults.baseURL = PROTOCOL + '://' + ADDRESS + ':' + PORT
+let baseUrl = PROTOCOL + '://' + ADDRESS + ':' + PORT
+axios.defaults.baseURL = baseUrl
 
 // axios的全局配置
 // axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
-axios.defaults.headers.common['Authorization'] = sessionStorage.getItem('sztu_doc_token')
+//axios.defaults.headers.common['Authorization'] = sessionStorage.getItem('sztu_doc_token')
+axios.interceptors.request.use(
+  config => {
+    // 判断是否存在token，如果存在的话，则每个http header都加上token
+    let token = sessionStorage.getItem('sztu_doc_token')
+    config.headers.Authorization = token;
+    return config;
+  },
+  error => {
+    return Promise.reject(error);
+  });
 
-let baseUrl = PROTOCOL + '://' + ADDRESS + ':' + PORT
+
 let base = {
   login: '/User/Login',
   register: '/User/Register',
